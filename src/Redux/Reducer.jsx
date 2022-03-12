c375ea98c24b36d14de930c8ea1d748d2df17249
ArleysGatica <gatica.arleys16@gmail.com>
2022-03-11 18:09:45 -0600

    intentandolo

--- src/Redux/Reducer.jsx
+++ src/Redux/Reducer.jsx
@@ -1,17 +1,52 @@
-import { createSlece } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [];
 
-const addTodoReducer = createSlece({
-  name: 'addTodo',
+const addTodoReducer = createSlice({
+  name: "todos",
   initialState,
   reducers: {
-    addTodo: (state, action) => {
+    //here we will write our reducer
+    //Adding todos
+    addTodos: (state, action) => {
       state.push(action.payload);
       return state;
-    }
-  }
-})
+    },
+    //remove todos
+    removeTodos: (state, action) => {
+      return state.filter((item) => item.id !== action.payload);
+    },
+    //update todos
+    updateTodos: (state, action) => {
+      return state.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return {
+            ...todo,
+            item: action.payload.item,
+          };
+        }
+        return todo;
+      });
+    },
+    //completed
+    completeTodos: (state, action) => {
+      return state.map((todo) => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            completed: true,
+          };
+        }
+        return todo;
+      });
+    },
+  },
+});
 
-export const { addTodo } = addTodoReducer.actions;
+export const {
+  addTodos,
+  removeTodos,
+  updateTodos,
+  completeTodos,
+} = addTodoReducer.actions;
 export const reducer = addTodoReducer.reducer;
